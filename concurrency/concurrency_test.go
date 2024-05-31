ff1f605ef939c1c79b726f1c13af09955608414b
Akhilesh Sonone <akkhi.sonone@gmail.com>
2024-05-31 22:52:50 +0530

    write benchmark for CheckWebsites function

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -32,3 +32,20 @@ func TestCheckWebsites(t *testing.T) {
 		t.Fatalf("wanted %v, got %v", want, got)
 	}
 }
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsite(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
